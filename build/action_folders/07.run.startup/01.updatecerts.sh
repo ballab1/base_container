0b2c4dd02b5299de3d334e00a1128884070464af
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-10-07 12:00:00 -0400

    updated build.sh for non-alpine OSs

--- build/action_folders/07.run.startup/01.updatecerts.sh
+++ build/action_folders/07.run.startup/01.updatecerts.sh
@@ -6,32 +6,37 @@ declare -r cert_home=/usr/local/share/ca-certificates
 # Update certs on system
 #############################################################################
 # copy kubernetes read-only mounted secrets over to expected directory
-cp -f /mnt/data/ca-certificates/* ${cert_home}/ ||:
+if [ -d /mnt/data/ca-certificates ]; then
+    cp -f /mnt/data/ca-certificates/* "${cert_home}/" ||:
+fi
 
 # load certs via system
-update-ca-certificates -f
+case "$( environ.OSid )" in
+    alpine)
+        update-ca-certificates -f ||:
+        ;;
+    centos|fedora)
+        update-ca-trust extract ||:
+        ;;
+    ubuntu)
+        ;;
+esac
 
 # skip the rest if Java is not installed with keytool
-if ! type -f keytool &> /dev/null; then
-    exit 0
-fi
+[ "${JAVA_HOME:-}" ] || exit 0
+type -f keytool &> /dev/null || exit 0
 
 #############################################################################
 # Continue onwards for java cacert
 #############################################################################
 
-# add custom SSL if found in /usr/local/share/ca-certificates
-declare -r jre_home=$( readlink -f $(dirname $(readlink -f $(which java)))/.. )
-export JAVA_HOME=$jre_home
-export PATH="${PATH}:$JAVA_HOME/bin"
-
-function keytool_cmd() {
-    keytool -noprompt -storepass changeit -keystore "${JAVA_HOME}/lib/security/cacerts" "$@"
-}
-
-for cert in $cert_home/*; do
-    aliasname=$(basename $cert)
-    aliasname=DellEMC_${aliasname%.*}
+while read -r cert; do
+    aliasname=DellEMC_${cert%.*}
     echo "$aliasname -> $cert into ${JAVA_HOME}"
-    keytool_cmd -import -alias "$aliasname" -file ${cert} || echo "Failed to load $cert into JVM cacert"
-done
+    keytool -noprompt \
+            -storepass changeit \
+            -keystore "${JAVA_HOME}/lib/security/cacerts" \
+            -import -alias "$cert" \
+            -file "${cert_home}/${cert}" \
+    || echo "Failed to load $cert into JVM cacert"
+done < <(cd "${cert_home}"; ls -1A)
